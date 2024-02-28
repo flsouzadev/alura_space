e0cb65786e3f98c774d249b5edeeb6a9143f6c8a
Flavio Lopes De Souza <flsouza@cpqd.com.br>
2024-02-28 12:02:42 -0300

    finalizando o projeto

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 
-from usuarios.forms import LoginForms, CadastroForms
+from apps.usuarios.forms import LoginForms, CadastroForms
 
 from django.contrib.auth.models import User
 
 from django.contrib import auth
 
 from django.contrib import messages
+
 
 def login(request):
     form = LoginForms()
@@ -25,7 +26,7 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request, usuario)
-            messages.success(request, f'{nome} logado com sucesso!')
+            messages.success(request, f'{nome} login efetuado com sucesso!')
             return redirect('index')
         else:
             messages.error(request, 'Erro ao efetuar login')
